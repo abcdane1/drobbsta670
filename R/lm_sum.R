b99aa0ec186f3b94e3d5126f9be7dff5c429fc4b
abcdane1 <dane.isenberg@pennmedicine.upenn.edu>
2023-04-26 16:53:58 -0400

    Initial commit

---
+++ R/lm_sum.R
@@ -0,0 +1,19 @@
+#' Linear Regression
+#'
+#' Provides a Summary Output of Regression Model
+#'
+#' @param y dependent variable vector "y"
+#' @param x independent variable matrix "x" 
+#'
+#' @return lm summary
+#'
+#' @examples
+#'
+#' @export
+
+
+lm_sum<-function(y,x){
+  fit<-lm(y~x)
+  return(summary(fit))
+}
+
