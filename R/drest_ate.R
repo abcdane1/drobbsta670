ff380d57a213f53f3d7d0fced9fa58dbee159ea6
abcdane1 <dane.isenberg@pennmedicine.upenn.edu>
2023-04-29 21:08:22 -0400

    package description

--- R/drest_ate.R
+++ R/drest_ate.R
@@ -1,11 +1,20 @@
-#' Linear Regression
+#' Double Robust Estimation of ATE 
 #'
-#' Provides a Summary Output of Regression Model
+#' This package provides a doubly robust point estimate and confidence interval for the average treatment effect (ATE) with binary treatment. The outcome models are assumed to be linear, where
+#' the outcomes for those under treatment 1 and the outcomes for those under treatment 0 are regressed against user specified subsets of non-treatment covariates. The propensity score is modeled using logistic regression, 
+#' where treatment status is regressed against a subset of non-treatment user-specified covariates. For confidence intervals, the user has the option to specify whether they wish to have the asymptotic interval, 
+#' the basic (or empirical) bootstrap, or the percentile bootstrap.
+#' 
 #'
-#' @param y dependent variable vector "y"
-#' @param x independent variable matrix "x" 
+#' @param trt  A vector of binary treatments
+#' @param xout A matrix, data frame, or vector of non-treatment covariates 
+#' @param var1 The numbers or names of columns to be included in outcome model for treatment 1
+#' @param var0 The numbers or names of columns to be included in outcome model for treatment 0
+#' @param yout A vector of outcome variables 
+#' @param varp The numbers or names of columns to be included in propensity score model 
+#' 
 #'
-#' @return lm summary
+#' @return The return value is an object of...
 #'
 #' @examples
 #'
